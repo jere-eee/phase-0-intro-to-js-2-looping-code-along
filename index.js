9a5b02029f86c8d6a44f1b4ab02afeb105ee6d79
Akanle Jeremy <akanlejeremy@gmail.com>
2024-09-12 16:55:25 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages;
+};
+
+function countDown(integer) {
+    let number = integer;
+    while (number >= 0) {
+        console.log(number);
+        --number;
+    }
+}
